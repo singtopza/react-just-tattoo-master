d2ab3edb1eb14daf64335fd474c878e66c6253d6
singtopza <59428798+singtopza@users.noreply.github.com>
2021-08-24 20:45:06 +0700

    Initial commit

---
+++ src/components/AppHeader.js
@@ -0,0 +1,11 @@
+import './AppHeader.css';
+
+function AppHeader() {
+    return (
+        <header className="app-header">
+            <h4>Saran Wringsimma</h4>
+        </header>
+    );
+}
+
+export default AppHeader;
